f582d69a92ff3a1009fa901c13dc166bdf337d88
Kunal Singh <kunalsin9h@gmail.com>
2023-11-18 00:29:46 +0530

    update license presence

--- cmd/meltcd/server.go
+++ cmd/meltcd/server.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 - PRESENT Meltred Pvt. Ltd.
+Copyright 2023 - PRESENT Meltred
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
