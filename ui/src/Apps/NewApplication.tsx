69c6fd7613772d4227f70ed77fb024785688b400
Kunal Singh <kunalsin9h@gmail.com>
2024-01-01 21:58:00 +0530

    fix: lint

--- ui/src/Apps/NewApplication.tsx
+++ ui/src/Apps/NewApplication.tsx
@@ -18,6 +18,7 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { CloseIcon } from "../lib/icon";
 import { useNavigate } from "react-router-dom";
+import normalizeInput from "../utils/normalizeInput";
 
 type globalResponseData = {
   message: string;
@@ -276,32 +277,4 @@ function InputOption({
       />
     </label>
   );
-}
-
-export function normalizeInput(
-  givenText: string,
-  allowed: string[],
-  toastBottom?: boolean
-): string {
-  const len = givenText.length;
-  let result = "";
-
-  for (let i = 0; i < len; i++) {
-    const code = givenText.charCodeAt(i);
-
-    if (
-      (code > 47 && code < 58) || // numeric (0-9)
-      (code > 64 && code < 91) || // upper alpha (A-Z)
-      (code > 96 && code < 123) || // lower alpha (a-z)
-      allowed.includes(givenText[i])
-    ) {
-      result += givenText[i];
-    } else {
-      toast.error(`${givenText[i]} is not allowed in input here!`, {
-        position: toastBottom ? "bottom-right" : "top-center",
-      });
-    }
-  }
-
-  return result;
 }
