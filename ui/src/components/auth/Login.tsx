786c248e8df3bd4d575080700c508b38d45a04a8
Kunal Singh <kunalsin9h@gmail.com>
2024-01-02 12:07:54 +0530

    fix: ui of login

--- ui/src/components/auth/Login.tsx
+++ ui/src/components/auth/Login.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
   return (
     <div className="h-screen bg-rootBg flex items-center justify-center">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-        <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <div className="flex items-center mb-6 text-2xl font-semibold text-white">
           <img
             className="w-8 h-8 mr-2 select-none"
             src="/logo.png"
@@ -30,7 +30,7 @@ export default function LoginPage() {
               <div>
                 <label
                   htmlFor="username"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-white"
                 >
                   Your Username
                 </label>
@@ -38,7 +38,7 @@ export default function LoginPage() {
                   type="text"
                   name="username"
                   id="username"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="border sm:text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   placeholder="admin"
                   required={true}
                 />
@@ -46,7 +46,7 @@ export default function LoginPage() {
               <div>
                 <label
                   htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-white"
                 >
                   Password
                 </label>
@@ -55,7 +55,7 @@ export default function LoginPage() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="border sm:text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   required={true}
                 />
               </div>
