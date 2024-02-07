57350ef317ac3fb35ce28417d965d6d8df1e62e7
Kunal Singh <kunalsin9h@gmail.com>
2024-02-07 19:52:54 +0530

    fix: allow - in service file path

--- ui/src/Apps/NewApplication.tsx
+++ ui/src/Apps/NewApplication.tsx
@@ -21,57 +21,57 @@ import { useNavigate } from "react-router-dom";
 import normalizeInput from "../utils/normalizeInput";
 
 type globalResponseData = {
-  message: string;
+	message: string;
 };
 
 export default function NewApplication({
-  openWindow,
-  setOpenWindow,
-  setRefresh,
+	openWindow,
+	setOpenWindow,
+	setRefresh,
 }: {
-  openWindow: boolean;
-  setOpenWindow: React.Dispatch<React.SetStateAction<boolean>>;
-  setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
+	openWindow: boolean;
+	setOpenWindow: React.Dispatch<React.SetStateAction<boolean>>;
+	setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  {
-    /* Slider Window
+	{
+		/* Slider Window
         This is used to create a new application
        */
-  }
-  return (
-    <>
-      <div
-        className={`fixed z-50 overflow-auto  bg-white h-full w-[40%] top-0 left-[60%] p-4 text-black ${
-          openWindow ? "" : "hidden"
-        }`}
-      >
-        <div className="flex justify-between items-center">
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              setOpenWindow(false);
-            }}
-            className="hover:bg-red-200 p-1 rounded"
-          >
-            <CloseIcon />
-          </button>
-          <p className="text-xl">Creating a new Application</p>
-        </div>
-        <div className="h-full px-8 py-16">
-          <CreateApplication
-            setRefresh={setRefresh}
-            setOpenWindow={setOpenWindow}
-          />
-        </div>
-      </div>
-      {/* Overlay for Slider */}
-      <div
-        className={`fixed  h-full w-full top-0 left-0 bg-black/50 ${
-          openWindow ? "" : "hidden"
-        }`}
-      ></div>
-    </>
-  );
+	}
+	return (
+		<>
+			<div
+				className={`fixed z-50 overflow-auto  bg-white h-full w-[40%] top-0 left-[60%] p-4 text-black ${
+					openWindow ? "" : "hidden"
+				}`}
+			>
+				<div className="flex justify-between items-center">
+					<button
+						onClick={(e) => {
+							e.preventDefault();
+							setOpenWindow(false);
+						}}
+						className="hover:bg-red-200 p-1 rounded"
+					>
+						<CloseIcon />
+					</button>
+					<p className="text-xl">Creating a new Application</p>
+				</div>
+				<div className="h-full px-8 py-16">
+					<CreateApplication
+						setRefresh={setRefresh}
+						setOpenWindow={setOpenWindow}
+					/>
+				</div>
+			</div>
+			{/* Overlay for Slider */}
+			<div
+				className={`fixed  h-full w-full top-0 left-0 bg-black/50 ${
+					openWindow ? "" : "hidden"
+				}`}
+			></div>
+		</>
+	);
 }
 
 // USING api POST /api/apps
@@ -87,191 +87,195 @@ export default function NewApplication({
   } 
  */
 function CreateApplication({
-  setRefresh,
-  setOpenWindow,
+	setRefresh,
+	setOpenWindow,
 }: {
-  setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
-  setOpenWindow: React.Dispatch<React.SetStateAction<boolean>>;
+	setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
+	setOpenWindow: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const initialData = {
-    name: "",
-    refresh_timer: "3m0s",
-    source: {
-      path: "",
-      repoURL: "",
-      targetRevision: "HEAD",
-    },
-  };
+	const initialData = {
+		name: "",
+		refresh_timer: "3m0s",
+		source: {
+			path: "",
+			repoURL: "",
+			targetRevision: "HEAD",
+		},
+	};
 
-  const [bodyData, setBodyData] = useState(initialData);
+	const [bodyData, setBodyData] = useState(initialData);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-    const request = fetch("/api/apps", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bodyData),
-    });
+		const request = fetch("/api/apps", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(bodyData),
+		});
 
-    toast.promise(request, {
-      loading: "Creating new application",
-      success: (res) => {
-        let good = true;
-        if (res.status === 401) {
-          navigate("/login");
-        }
-        if (res.status !== 200) {
-          good = false;
-        }
+		toast.promise(request, {
+			loading: "Creating new application",
+			success: (res) => {
+				let good = true;
+				if (res.status === 401) {
+					navigate("/login");
+				}
+				if (res.status !== 200) {
+					good = false;
+				}
 
-        res
-          .json()
-          .then((data: globalResponseData) => {
-            if (good) {
-              toast.success(data.message);
-              // we are good here
-              // and refresh the app list
-              setRefresh(true);
-              // we can close modal window
-              setOpenWindow(false);
-            } else {
-              toast.error(data.message);
-            }
-          })
-          .catch((err) => {
-            console.log(err);
-            toast.error("Failed to create new application");
-          });
+				res.json()
+					.then((data: globalResponseData) => {
+						if (good) {
+							toast.success(data.message);
+							// we are good here
+							// and refresh the app list
+							setRefresh(true);
+							// we can close modal window
+							setOpenWindow(false);
+						} else {
+							toast.error(data.message);
+						}
+					})
+					.catch((err) => {
+						console.log(err);
+						toast.error("Failed to create new application");
+					});
 
-        return "Executing task";
-      },
-      error: (err) => {
-        console.log(err);
-        return "Failed to create new application";
-      },
-    });
-  };
+				return "Executing task";
+			},
+			error: (err) => {
+				console.log(err);
+				return "Failed to create new application";
+			},
+		});
+	};
 
-  return (
-    <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
-      <InputOption
-        name="Name"
-        placeholder="auth_backend_server"
-        value={bodyData.name}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setBodyData({
-            ...bodyData,
-            name: normalizeInput(e.target.value, ["_"]),
-          });
-        }}
-      />
-      <InputOption
-        name="Sync Timer"
-        placeholder="3m30s (Default, for 3 minute and 30 seconds)"
-        value={bodyData.refresh_timer}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setBodyData({
-            ...bodyData,
-            refresh_timer: normalizeInput(e.target.value, []),
-          });
-        }}
-      />
-      <InputOption
-        name="Repository URL"
-        placeholder="https://github.com/username/repo"
-        value={bodyData.source.repoURL}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setBodyData({
-            ...bodyData,
-            source: {
-              ...bodyData.source,
-              repoURL: normalizeInput(e.target.value, [
-                ":",
-                "/",
-                ".",
-                "_",
-                "-",
-              ]),
-            },
-          });
-        }}
-      />
-      <InputOption
-        name="Service File Path"
-        placeholder="deploy/service.yaml"
-        value={bodyData.source.path}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setBodyData({
-            ...bodyData,
-            source: {
-              ...bodyData.source,
-              path: normalizeInput(e.target.value, [".", "/", "_"]),
-            },
-          });
-        }}
-      />
-      <InputOption
-        name="Target Revision"
-        placeholder="HEAD (Default, can be master, main, my_branch)"
-        value={bodyData.source.targetRevision}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setBodyData({
-            ...bodyData,
-            source: {
-              ...bodyData.source,
-              targetRevision: normalizeInput(e.target.value, []),
-            },
-          });
-        }}
-      />
-      <div className="flex items-center gap-4">
-        <input
-          className="text-black py-2 px-4 rounded font-bold bg-green-400 hover:bg-green-500 cursor-pointer"
-          value="Create"
-          type="submit"
-        />
-        <input
-          className="text-black py-2 px-4 rounded font-bold border hover:bg-gray-100 border-1 border-black cursor-pointer"
-          onClick={(e) => {
-            e.preventDefault();
-            setBodyData(initialData);
-            toast.success("Input data reset");
-          }}
-          type="button"
-          value="Clear"
-        />
-      </div>
-    </form>
-  );
+	return (
+		<form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+			<InputOption
+				name="Name"
+				placeholder="auth_backend_server"
+				value={bodyData.name}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+					setBodyData({
+						...bodyData,
+						name: normalizeInput(e.target.value, ["_"]),
+					});
+				}}
+			/>
+			<InputOption
+				name="Sync Timer"
+				placeholder="3m30s (Default, for 3 minute and 30 seconds)"
+				value={bodyData.refresh_timer}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+					setBodyData({
+						...bodyData,
+						refresh_timer: normalizeInput(e.target.value, []),
+					});
+				}}
+			/>
+			<InputOption
+				name="Repository URL"
+				placeholder="https://github.com/username/repo"
+				value={bodyData.source.repoURL}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+					setBodyData({
+						...bodyData,
+						source: {
+							...bodyData.source,
+							repoURL: normalizeInput(e.target.value, [
+								":",
+								"/",
+								".",
+								"_",
+								"-",
+							]),
+						},
+					});
+				}}
+			/>
+			<InputOption
+				name="Service File Path"
+				placeholder="deploy/service.yaml"
+				value={bodyData.source.path}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+					setBodyData({
+						...bodyData,
+						source: {
+							...bodyData.source,
+							path: normalizeInput(e.target.value, [
+								".",
+								"/",
+								"_",
+								"-",
+							]),
+						},
+					});
+				}}
+			/>
+			<InputOption
+				name="Target Revision"
+				placeholder="HEAD (Default, can be master, main, my_branch)"
+				value={bodyData.source.targetRevision}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+					setBodyData({
+						...bodyData,
+						source: {
+							...bodyData.source,
+							targetRevision: normalizeInput(e.target.value, []),
+						},
+					});
+				}}
+			/>
+			<div className="flex items-center gap-4">
+				<input
+					className="text-black py-2 px-4 rounded font-bold bg-green-400 hover:bg-green-500 cursor-pointer"
+					value="Create"
+					type="submit"
+				/>
+				<input
+					className="text-black py-2 px-4 rounded font-bold border hover:bg-gray-100 border-1 border-black cursor-pointer"
+					onClick={(e) => {
+						e.preventDefault();
+						setBodyData(initialData);
+						toast.success("Input data reset");
+					}}
+					type="button"
+					value="Clear"
+				/>
+			</div>
+		</form>
+	);
 }
 
 interface InputOptionProps {
-  name: string;
-  placeholder: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	name: string;
+	placeholder: string;
+	value: string;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 function InputOption(props: InputOptionProps) {
-  const id = props.name.replace(" ", "_");
+	const id = props.name.replace(" ", "_");
 
-  return (
-    <label htmlFor={id} className="flex flex-col">
-      <span className="font-semibold my-1">{props.name}</span>
-      <input
-        id={id}
-        required={true}
-        className="border p-1 rounded px-2 bg-white"
-        type="text"
-        placeholder={props.placeholder}
-        value={props.value}
-        onChange={props.onChange}
-      />
-    </label>
-  );
+	return (
+		<label htmlFor={id} className="flex flex-col">
+			<span className="font-semibold my-1">{props.name}</span>
+			<input
+				id={id}
+				required={true}
+				className="border p-1 rounded px-2 bg-white"
+				type="text"
+				placeholder={props.placeholder}
+				value={props.value}
+				onChange={props.onChange}
+			/>
+		</label>
+	);
 }
